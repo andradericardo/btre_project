cf20533a91845cfc6922b5c52d42ec4b734a45c0
Ricardo Andrade <ricardoandrade@RicardoradesMBP.home>
2018-12-06 02:20:30 -0200

    fixes in models

--- btre/settings.py
+++ btre/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
