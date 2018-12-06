5297718f6d2ce7293e1280ee0799ae4358070120
Ricardo Andrade <ricardoandrade@RicardoradesMBP.home>
2018-12-06 03:09:03 -0200

    created migrations

--- btre/settings.py
+++ btre/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
