540003f4a4cd6fc4098aa624978c92fed0652c46
Ricardo Andrade <ricardoandrade@RicardoradesMBP.home>
2018-12-12 22:55:50 -0200

    Dashboard done

--- btre/settings.py
+++ btre/settings.py
@@ -147,5 +147,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER='oliveira.andrade@gmail.com'
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_PASSWORD='Randr.011'
 EMAIL_USE_TLS=True
